4f987ef302ddc71a9f33e4d6776fd09214c6cc5b
Ajinkyap22 <palaskarap15@gmail.com>
2021-03-12 20:41:26 +0530

    added classes & responsive layout

--- script.js
+++ script.js
@@ -1,81 +1,218 @@
 "use strict";
 
-// VARIABLES
+class Player {
+  constructor(sign, type) {
+    this.sign = sign;
+    this.type = type;
+  }
 
-const grids = document.querySelectorAll(".grid");
-const title = document.querySelector(".title");
-// store grid states in an array
-const states = Array(9).fill("");
-const chips = ["X", "O"];
-// store turn in a variable
-let turn = 0;
-// store game state in a variable
-let isPlaying = true;
+  getSign() {
+    return this.sign;
+  }
 
-// EVENT LISTENERS
+  isBot() {
+    return this.type === "Computer";
+  }
 
-grids.forEach((grid) => grid.addEventListener("click", play));
+  setType(newType) {
+    this.type = newType;
+  }
 
-document.querySelector(".reset").addEventListener("click", reset);
-
-// FUNCTIONS
-
-function play(e) {
-  // If the grid is already taken then return
-  if (this.textContent !== "" || !isPlaying) return;
-
-  // Draw on the grid
-  this.textContent = chips[turn];
-  this.classList.add(chips[turn]);
-
-  // fill the states array for the given grid
-  const index = this.dataset.index;
-  states[index] = chips[turn];
-
-  check(turn);
-}
-
-// check for diff scenarios
-function check(player) {
-  // check for win
-  if (
-    // horrizontal check
-    (states[0] === states[1] && states[1] === states[2] && states[0]) ||
-    (states[3] === states[4] && states[4] === states[5] && states[3]) ||
-    (states[6] === states[7] && states[7] === states[8] && states[6]) ||
-    // vertical check
-    (states[0] === states[3] && states[3] === states[6] && states[0]) ||
-    (states[1] === states[4] && states[4] === states[7] && states[1]) ||
-    (states[2] === states[5] && states[5] === states[8] && states[2]) ||
-    // diagonal check
-    (states[0] === states[4] && states[4] === states[8] && states[0]) ||
-    (states[2] === states[4] && states[4] === states[6] && states[2])
-  ) {
-    title.textContent = `Player ${player + 1} Wins!`;
-    grids.forEach((grid) => grid.classList.add("game-over"));
-    isPlaying = false;
-
-    // check for draw
-  } else if (states.every((state) => state)) {
-    title.textContent = `It's a Tie!`;
-    grids.forEach((grid) => grid.classList.add("game-over"));
-    isPlaying = false;
-  } else {
-    // switch player
-    turn = turn === 0 ? 1 : 0;
+  getType() {
+    return this.type;
   }
 }
 
-// reset function
-function reset() {
-  states.fill("");
-  turn = 0;
-  isPlaying = true;
-  grids.forEach((grid) => {
-    grid.textContent = "";
-    grid.classList.remove("X");
-    grid.classList.remove("O");
-    grid.classList.remove("game-over");
-  });
-  title.textContent = `Let's Play Tic Tac Toe!`;
+class GameBoard {
+  states;
+  constructor() {
+    this.states = Array(9).fill("");
+  }
+
+  setState(index, sign) {
+    this.states[index] = sign;
+  }
+
+  resetStates() {
+    this.states.fill("");
+  }
 }
+
+class ComputerPlay {
+  getMove() {
+    // get a random index from 0 to 8
+    const move = Math.floor(Math.random() * 9);
+    if (gameBoard.states[move] !== "") {
+      // try again if index is already filled
+      return this.getMove();
+    }
+    return move;
+  }
+}
+
+class DisplayController {
+  grids = document.querySelectorAll(".grid");
+  gameOverModal = document.querySelector(".game__over");
+  overlay = document.querySelector(".overlay");
+  labelTurn = document.querySelector(".turn");
+  resultLabel = document.querySelector(".result");
+  playerLabel = document.querySelector(".player__label");
+  btnSinglePlayer = document.querySelector(".single");
+  btnMultiPlayer = document.querySelector(".multi");
+
+  constructor() {
+    this.grids.forEach((grid) =>
+      grid.addEventListener("click", gameController.play)
+    );
+
+    this.btnSinglePlayer.onclick = this.switchMode.bind(this);
+
+    this.btnMultiPlayer.onclick = this.switchMode.bind(this);
+    document
+
+      .querySelectorAll(".btn")
+      .forEach((btn) => btn.addEventListener("click", gameController.reset));
+  }
+
+  updateBoard(field, sign) {
+    field.textContent = sign;
+    field.classList.add(sign);
+  }
+
+  // change current player label after each turn
+  SwitchTurnLabel(newPlayer) {
+    this.labelTurn.textContent = `${newPlayer}'s turn`;
+  }
+
+  switchMode(e) {
+    if (e.target.classList.contains("active")) return;
+
+    gameController.reset();
+
+    this.btnMultiPlayer.classList.remove("active");
+    this.btnSinglePlayer.classList.remove("active");
+
+    gameController.multiplayer = !gameController.multiplayer;
+
+    e.target.classList.add("active");
+
+    if (e.target.textContent === "Singleplayer") {
+      this.playerLabel.textContent = "Computer";
+      gameController.playerO.setType("Computer");
+    } else {
+      this.playerLabel.textContent = "Player O";
+      gameController.playerO.setType("Player O");
+    }
+  }
+
+  resetBoard() {
+    this.grids.forEach((grid) => {
+      grid.textContent = "";
+      grid.classList.remove("X");
+      grid.classList.remove("O");
+    });
+    this.gameOverModal.classList.add("hidden");
+    this.overlay.classList.add("hidden");
+    this.SwitchTurnLabel("Player X");
+  }
+
+  gameOver(winner) {
+    if (winner === "draw") {
+      this.resultLabel.textContent = "Its a Tie!";
+    } else {
+      this.resultLabel.textContent = `${winner} Has Won!!`;
+    }
+
+    this.gameOverModal.classList.remove("hidden");
+    this.overlay.classList.remove("hidden");
+  }
+}
+
+const gameController = (function () {
+  const playerX = new Player("X", "Player X");
+  let playerO = new Player("O", "Player O");
+  let currentPlayer = playerX;
+  let isPlaying = true;
+
+  function play() {
+    if (this.textContent !== "" || !isPlaying) return;
+
+    // Multi player
+    const index = +this.dataset.index;
+
+    displayController.updateBoard(this, currentPlayer.getSign());
+    gameBoard.setState(index, currentPlayer.getSign());
+
+    check(index);
+
+    // Single player
+    if (playerO.isBot()) {
+      isPlaying = false;
+
+      if (gameBoard.states.every((state) => state !== "")) return;
+
+      const botMove = computerPlay.getMove();
+      const botField = document.querySelector(`[data-index="${botMove}"]`);
+
+      setTimeout(() => {
+        displayController.updateBoard(botField, "O");
+        isPlaying = true;
+      }, 700);
+
+      gameBoard.setState(botMove, "O");
+
+      setTimeout(() => check(botMove), 800);
+    }
+  }
+
+  function check(index) {
+    const winConditions = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    // check for win
+    if (
+      winConditions
+        .filter((condition) => condition.includes(index))
+        .some((condition) =>
+          condition.every(
+            (i) => gameBoard.states[i] === currentPlayer.getSign()
+          )
+        )
+    ) {
+      displayController.gameOver(currentPlayer.getType());
+      isPlaying = false;
+
+      // check for draw
+    } else if (gameBoard.states.every((state) => state)) {
+      displayController.gameOver("draw");
+      isPlaying = false;
+    } else {
+      // switch player
+      currentPlayer = currentPlayer === playerX ? playerO : playerX;
+      displayController.SwitchTurnLabel(currentPlayer.getType());
+    }
+  }
+
+  function reset() {
+    gameBoard.resetStates();
+    currentPlayer = playerX;
+    displayController.resetBoard();
+    isPlaying = true;
+  }
+
+  return { playerO, play, check, reset };
+})();
+
+const gameBoard = new GameBoard();
+
+const computerPlay = new ComputerPlay();
+
+const displayController = new DisplayController();
